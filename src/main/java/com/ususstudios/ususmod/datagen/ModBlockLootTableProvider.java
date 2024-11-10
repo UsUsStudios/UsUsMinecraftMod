35416d39735c1b48a1e44beffe65359275a4da4e
UsUsStudios <biruni056@gmail.com>
2024-11-09 18:57:53 -0700

    ModBlockLootTableProvider

--- src/main/java/com/ususstudios/ususmod/datagen/ModBlockLootTableProvider.java
+++ src/main/java/com/ususstudios/ususmod/datagen/ModBlockLootTableProvider.java
@@ -1,5 +1,53 @@
 package com.ususstudios.ususmod.datagen;
 
-public class ModBlockLootTableProvider {
+import com.ususstudios.ususmod.block.ModBlocks;
+import com.ususstudios.ususmod.item.ModItems;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+	
+	protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+		super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+	}
+	
+	@Override
+	protected void generate() {
+		dropSelf(ModBlocks.HEMATITE_BLOCK.get());
+		dropSelf(ModBlocks.HEMATITE_PROCESSOR.get());
+		
+		add(ModBlocks.HEMATITE_ORE.get(),
+				block -> createMultipleOreDrops(ModBlocks.HEMATITE_ORE.get(), ModItems.RAW_HEMATITE.get(), 2, 4));
+		add(ModBlocks.HEMATITE_DEEPSLATE_ORE.get(),
+				block -> createMultipleOreDrops(ModBlocks.HEMATITE_DEEPSLATE_ORE.get(), ModItems.RAW_HEMATITE.get(),
+						2, 5));
+	}
+	
+	protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+		HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+		return this.createSilkTouchDispatchTable(pBlock,
+				this.applyExplosionDecay(pBlock, LootItem.lootTableItem(item)
+						.apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+						.apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
+	}
+	
+	@Override
+	protected Iterable<Block> getKnownBlocks() {
+		return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+	}
 }
